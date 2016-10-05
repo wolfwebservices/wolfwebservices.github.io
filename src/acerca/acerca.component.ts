d86cf6b6c4eab6a21556ef342cb5d66eb183a6b3
Wolf Web Services <wolfwebservices.corp@gmail.com>
2016-10-05 03:09:46 -0500

    Angular 2 version
    whoa angular 2 version amazing

---
+++ src/acerca/acerca.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+  selector: 'Acerca',
+  templateUrl: 'src/acerca/acerca.html'
+})
+export class AcercaComponent {
+
+}
---
+++ src/inicio/inicio.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+  selector: 'Inicio',
+  templateUrl: 'src/inicio/inicio.html'
+})
+export class InicioComponent {
+
+}
