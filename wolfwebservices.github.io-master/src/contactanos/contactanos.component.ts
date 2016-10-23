9a73a1ff0b12d199455bdbae81adc93c2229855b
Wolf Web Services <wolfwebservices.corp@gmail.com>
2016-10-23 15:22:56 -0500

    Please work !

---
+++ wolfwebservices.github.io-master/src/contactanos/contactanos.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+  selector: 'Contactanos',
+  templateUrl: 'src/contactanos/contactanos.html'
+})
+export class ContactanosComponent {
+
+}
---
+++ wolfwebservices.github.io-master/src/portafolio/portafolio.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+  selector: 'Portafolio',
+  templateUrl: 'src/portafolio/portafolio.html'
+})
+export class PortafolioComponent {
+
+}
